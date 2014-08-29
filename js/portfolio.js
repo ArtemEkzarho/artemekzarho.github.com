45a6ca9b6cc2983d5dc9d8e182db295b423b1e5c
ArtemEkzarho <sru_529@mail.ru>
2014-08-29 16:39:24 +0300

    add functional for tabs

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,11 +1,12 @@
 	'use strict';
 
 	var sideTabs = $('.side-tabs');
+	var mainWrap = $('.main-wrap');
 	var portfolio = $('.portfolio-main');
 	var mainConfig;
 	var responses;
 	var divContent;
-	var topTabs;
+	var topTabs = $('.portfolio.content-wrap.task1').children().first();
 
 	function getMainConfig() {
     	return $.getJSON('/config.json')
@@ -20,20 +21,85 @@
 	    })
 	}
 
+	//подгружает все описания для нужного задания
+	function getDescription(task) {
+		return $.getJSON('/tasks/' + task + '/config.json').then(function (fls) {
+			fls = fls.files;
+			return $.when.apply($, fls.map(function (taskDesc) {
+				console.log($.get('/tasks/' + task + '/'+ taskDesc));
+				return $.get('/tasks/' + task + '/'+ taskDesc)
+			}));
+		})
+	}
+
+
 	getAllConfigs()
 		.then(function (configs) {
 			var title, 
 				li,
-				primaryDiv;
+				div;
 			
 			responses = arguments;
-			//создаем левые табики
+			//создаем левые табики, верхние и вообще все
 			for (var i = 0; i < responses.length; i += 1) {
 				title = responses[i][0].title;
-				li = $('<li></li>');
+				li = $('<li></li>').addClass('task' + (i + 1));
 				li.appendTo(sideTabs).text(title);
+
+				divContent = $('<div class = "portfolio content-wrap hide">\
+									<ul class="top-tabs noselect">\
+									</ul>\
+								</div>').addClass('task' + (i + 1));
+
+				mainWrap.append(divContent);
+				topTabs = divContent.children();
+
+				for (var j = 0; j < responses[i][0].files.length; j += 1){
+					title = responses[i][0].files[j];
+					li = $('<li></li>').addClass('file' + (j + 1));
+					div = $('<div class = "content hide"><p></p></div>').addClass('file' + (j + 1));
+					div.appendTo(divContent);
+					li.appendTo(topTabs).text(title);
+				}
 			}
 			
-			sideTabs.children().first().addClass('select');			
-			$(primaryDiv).removeClass('hide');
+			sideTabs.children().first().addClass('select');		
+			$('.content-wrap').first().removeClass('hide');
+			$('.content').first().removeClass('hide');
+			$('.top-tabs li').first().addClass('select');
 		});
+
+	function selectSideTab() {
+		var target = $(this);
+		var targetCls = target.context.className;
+		var selectContent = $('.content-wrap.' + targetCls);
+
+		if(target.hasClass('select')) return;
+
+		sideTabs.children().removeClass('select');
+		target.addClass('select');
+
+		$('.content-wrap').addClass('hide');
+		selectContent.removeClass('hide');
+
+		selectContent.find('li').first().addClass('select');
+		selectContent.find('.content').addClass('hide');
+		selectContent.find('.content').first().removeClass('hide');
+	}
+
+	function selectTopTab() {
+		var target = $(this);
+		var targetCls = target.context.className;
+		var selectFile = $('.content.' + targetCls);
+
+		if(target.hasClass('select')) return;
+
+		mainWrap.find('li').removeClass('select');
+		target.addClass('select');
+
+		$('.content').addClass('hide');
+		selectFile.removeClass('hide');
+	}
+
+	sideTabs.on('click', 'li', selectSideTab);
+	mainWrap.on('click', 'li', selectTopTab);
