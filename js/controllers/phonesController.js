f3b39445c0ae8c740125e487091e28c8bbd46110
Artem <artem.ekzarho@gmail.com>
2015-10-26 20:40:53 +0200

    test

--- js/controllers/phonesController.js
+++ js/controllers/phonesController.js
@@ -1,34 +1,22 @@
-'use strict';
-
 var myApp=angular.module('myApp');
 myApp.controller('phoneController', function($scope) {
-  
-    $scope.phones = [{
+ 
+  $scope.phones = [{
         name: 'Nokia Lumia 630',
         year: 2014,
         price: 200,
-        company: {
-            name: 'Nokia',
-            country: 'Финляндия'
-        }
+        company: 'Nokia'
     },{
         name: 'Samsung Galaxy S 4',
         year: 2014,
         price: 400,
-        company: {
-            name: 'Samsung',
-            country: 'Республика Корея'
-        }
+        company: 'Samsung'
     },{
         name: 'Mi 5',
         year: 2015,
         price: 300,
-        company: {
-            name: 'Xiaomi',
-            country: 'Китай'
-        }
+        company: 'Xiaomi'
     }];
-
     $scope.tablets = [{
         name: 'Samsung Galaxy Tab S4',
         year: 2014,
@@ -45,21 +33,18 @@ myApp.controller('phoneController', function($scope) {
         price: 220,
         company: 'Lenovo'
     }];
-
-    $scope.data = {};
-    $scope.setFile = function () {
-        if ($scope.data.mode = 'Tablets') {
-            return 'tabletsList.html';
-        } else {
-            return 'phonesList.html';
-        }
-    };
-
-    $scope.modes = [{
-            value: 'Tablets',
-            label: 'Планшеты'
-        },{
-            value: 'Phones',
-            label: 'Смартфоны'
-    }];
+  $scope.data = {};
+  $scope.setFile = function () {
+    if($scope.data.mode=='Tablets')
+        return 'tabletsList.html';
+    else if($scope.data.mode=='Phones')
+        return 'phonesList.html';
+};
+  $scope.modes = [{
+      value: 'Tablets',
+      label: 'Планшеты'
+  },{
+      value: 'Phones',
+      label: 'Смартфоны'
+  }];
 });
