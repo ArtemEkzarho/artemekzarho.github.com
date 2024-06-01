cf92d37076cdd9fcb2b8cf193913ee0395c5b027
art <artem@reply.io>
2024-06-01 17:54:50 +0300

    remove old code

--- todo-react-ts/src/components/App/data.ts
+++
@@ -1,7 +0,0 @@
-import { nanoid } from 'nanoid';
-
-export const todoData = [
-  { label: 'Drink coffee', important: false, done: false, id: nanoid() },
-  { label: 'Make Awesome app', important: true, done: false, id: nanoid() },
-  { label: 'Have lunch', important: false, done: true, id: nanoid() },
-];
